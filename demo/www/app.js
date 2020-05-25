28d23d85de918f9837fd70e2fb9ee0c7a3f2cb60
Chris E. Kelley <ckelley@rti.org>
2020-05-25 16:55:43 -0500

    Initial commit

---
+++ demo/www/app.js
@@ -0,0 +1,336 @@
+// Copyright 2020-present Christopher J. Brody <chris.brody+brodybits@gmail.com>
+
+document.addEventListener('deviceready', onReady)
+
+function log (text) {
+  // log into the `messages` div:
+  document.getElementById('messages').appendChild(document.createTextNode(text))
+  document.getElementById('messages').appendChild(document.createElement('br'))
+  // and to the console
+  console.log(text)
+}
+
+const DATABASE_FILE_NAME = 'demo.db'
+
+// SQLITE_OPEN_READWRITE | SQLITE_OPEN_CREATE
+// ref: https://www.sqlite.org/c3ref/open.html
+const OPEN_DATABASE_FILE_FLAGS = 6
+
+// with 100 characters:
+const BIG_DATA_PATTERN_PART =
+  'abcdefghijklmnopqrstuvwxyz----' +
+  '1234567890123456789-' +
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ----' +
+  '1234567890123456789-'
+
+const BIG_DATA_PATTERN_FACTOR = 200
+
+// Seems to be OK without the pro-free enhancements:
+// const BIG_DATA_PATTERN_INSERT_COUNT = 1000
+// OK with the pro-free enhancements,
+// triggers OOM issue without the pro-free enhancements:
+const BIG_DATA_PATTERN_INSERT_COUNT = 2000
+
+function openMemoryDatabaseConnection (openCallback, errorCallback) {
+  window.sqliteBatchConnectionManager.openDatabaseConnection(
+    { fullName: ':memory:', flags: 2 },
+    openCallback,
+    errorCallback
+  )
+}
+
+function openFileDatabaseConnection (name, openCallback, errorCallback) {
+  window.sqliteStorageFile.resolveAbsolutePath(
+    {
+      name: name,
+      // TEMPORARY & DEPRECATED value, as needed for iOS & macOS ("osx"):
+      location: 2
+    },
+    function (path) {
+      log('database file path: ' + path)
+
+      window.sqliteBatchConnectionManager.openDatabaseConnection(
+        { fullName: path, flags: OPEN_DATABASE_FILE_FLAGS },
+        openCallback,
+        errorCallback
+      )
+    }
+  )
+}
+
+function openCacheFileDatabaseConnection (name, openCallback, errorCallback) {
+  window.resolveLocalFileSystemURL(
+    // portable across Android, iOS, & macOS ("osx"):
+    cordova.file.cacheDirectory,
+    function (entry) {
+      const dataDirectoryUrl = entry.toURL()
+
+      log('data directory url: ' + dataDirectoryUrl)
+
+      // hacky, working solution:
+      const path = dataDirectoryUrl.substring(7) + name
+
+      log('database cache file path: ' + path)
+
+      window.sqliteBatchConnectionManager.openDatabaseConnection(
+        { fullName: path, flags: OPEN_DATABASE_FILE_FLAGS },
+        openCallback,
+        errorCallback
+      )
+    }
+  )
+}
+
+function onReady () {
+  log('deviceready event received')
+  startMemoryDatabaseDemo()
+}
+
+function startMemoryDatabaseDemo () {
+  openMemoryDatabaseConnection(
+    function (id) {
+      log('memory database connection id: ' + id)
+
+      window.sqliteBatchConnectionManager.executeBatch(
+        id,
+        [['SELECT UPPER(?)', ['Text']]],
+        function (results) {
+          log(JSON.stringify(results))
+          startFileDatabaseDemo()
+        }
+      )
+    },
+    function (error) {
+      log('UNEXPECTED OPEN MEMORY DATABASE ERROR: ' + error)
+    }
+  )
+}
+
+function startFileDatabaseDemo () {
+  openFileDatabaseConnection(
+    DATABASE_FILE_NAME,
+    openDatabaseFileCallback,
+    function (e) {
+      log('UNEXPECTED OPEN ERROR: ' + e)
+    }
+  )
+}
+
+function openDatabaseFileCallback (connectionId) {
+  log('open connection id: ' + connectionId)
+
+  // ERROR TEST - file name with incorrect flags:
+  window.sqliteBatchConnectionManager.openDatabaseConnection(
+    { fullName: 'dummy.db', flags: 0 },
+    function (_ignored) {
+      log('FAILURE - unexpected open success callback received')
+    },
+    function (e) {
+      log('OK - received error callback as expected for incorrect open call')
+
+      // CONTINUE with batch demo, with the correct connectionId:
+      batchDemo(connectionId)
+    }
+  )
+}
+
+function batchDemo (connectionId) {
+  log('starting batch demo for connection id: ' + connectionId)
+  window.sqliteBatchConnectionManager.executeBatch(
+    connectionId,
+    [
+      [
+        'SELECT ?, -?, LOWER(?), UPPER(?)',
+        [null, 1234567.890123, 'ABC', 'Text']
+      ],
+      ['SELECT -?', [1234567890123456]], // should fit into 52 bits (signed)
+      ['SLCT 1', []],
+      ['SELECT ?', ['OK', 'out of bounds parameter']],
+      ['DROP TABLE IF EXISTS Testing', []],
+      ['CREATE TABLE Testing (data NOT NULL)', []],
+      ["INSERT INTO Testing VALUES ('test data')", []],
+      ['INSERT INTO Testing VALUES (null)', []],
+      ['DELETE FROM Testing', []],
+      ["INSERT INTO Testing VALUES ('test data 2')", []],
+      ["INSERT INTO Testing VALUES ('test data 3')", []],
+      ['SELECT * FROM Testing', []],
+      ["SELECT 'xyz'", []]
+    ],
+    batchCallback
+  )
+}
+
+function batchCallback (batchResults) {
+  // show batch results in JSON string format (on all platforms)
+  log('received batch results')
+  log(JSON.stringify(batchResults))
+
+  startReaderDemo()
+}
+
+function startReaderDemo () {
+  openFileDatabaseConnection(
+    DATABASE_FILE_NAME,
+    function (id) {
+      log('read from another connection id: ' + id)
+
+      window.sqliteBatchConnectionManager.executeBatch(
+        id,
+        [['SELECT * FROM Testing', []]],
+        function (res) {
+          log(JSON.stringify(res))
+          startCacheFileDemo()
+        }
+      )
+    },
+    function (error) {
+      log('UNEXPECTED OPEN ERROR: ' + error)
+    }
+  )
+}
+
+function startCacheFileDemo () {
+  openCacheFileDatabaseConnection(
+    DATABASE_FILE_NAME,
+    function (id) {
+      log('cache file database connection id: ' + id)
+
+      window.sqliteBatchConnectionManager.executeBatch(
+        id,
+        [
+          ['DROP TABLE IF EXISTS Testing', []],
+          ['CREATE TABLE Testing (data NOT NULL)', []],
+          ["INSERT INTO Testing VALUES ('test data')", []],
+          ['SELECT * FROM Testing', []]
+        ],
+        function (results) {
+          log(JSON.stringify(results))
+          startBigDataMemoryTest()
+        }
+      )
+    },
+    function (error) {
+      log('UNEXPECTED OPEN ERROR: ' + error)
+    }
+  )
+}
+
+function startBigDataMemoryTest () {
+  log('START BIG DATA test')
+
+  openMemoryDatabaseConnection(
+    function (connectionId) {
+      log('BIG DATA memory database test connection id: ' + connectionId)
+      continueBigDataMemoryTest(connectionId)
+    },
+    function (error) {
+      log('UNEXPECTED OPEN BIG DATA MEMORY DATABASE ERROR: ' + error)
+    }
+  )
+}
+
+function continueBigDataMemoryTest (connectionId) {
+  // A workaround is needed here since String.prototype.repeat()
+  // does not work on all supported Android versions
+  // at the time of adding this test.
+  // const BIG_DATA_PATTERN = BIG_DATA_PATTERN_PART.repeat(BIG_DATA_PATTERN_FACTOR)
+  var bigPattern = ''
+  for (var i = 0; i < BIG_DATA_PATTERN_FACTOR; ++i) {
+    bigPattern = bigPattern.concat(BIG_DATA_PATTERN_PART)
+  }
+  const BIG_DATA_PATTERN = bigPattern
+
+  var rowCount = 0
+
+  log('INSERT BIG DATA')
+
+  window.sqliteBatchConnectionManager.executeBatch(
+    connectionId,
+    [['CREATE TABLE BIG (DATA)', []]],
+    addRow
+  )
+
+  function addRow () {
+    ++rowCount
+    window.sqliteBatchConnectionManager.executeBatch(
+      connectionId,
+      [
+        ['INSERT INTO BIG VALUES (?)', [BIG_DATA_PATTERN + (100000 + rowCount)]]
+      ],
+      function () {
+        if (rowCount < BIG_DATA_PATTERN_INSERT_COUNT) {
+          addRow()
+        } else {
+          checkBigData()
+        }
+      }
+    )
+  }
+
+  function checkBigData () {
+    log('CHECK info stored in BIG data table')
+    window.sqliteBatchConnectionManager.executeBatch(
+      connectionId,
+      [
+        ['SELECT COUNT(*) FROM BIG', []],
+        ['SELECT DATA FROM BIG', []]
+      ],
+      function (results) {
+        log('SELECT BIG DATA OK')
+        log('SELECT COUNT results: ' + JSON.stringify(results[0]))
+        log('SELECT BIG DATA rows length: ' + results[0].rows.length)
+        extraCheck1()
+      }
+    )
+  }
+
+  function extraCheck1 () {
+    log('EXTRA CHECK 1')
+    window.sqliteBatchConnectionManager.executeBatch(
+      connectionId,
+      [
+        ['SELECT COUNT(*) FROM BIG', []],
+        ['SELECT UPPER(?)', ['Extra test 1']],
+        ['SELECT DATA FROM BIG', []],
+        ['SELECT LOWER(?)', ['Extra test 2']]
+      ],
+      function (results) {
+        log('SELECT COUNT results: ' + JSON.stringify(results[0]))
+        log('EXTRA SELECT 1 result: ' + JSON.stringify(results[1]))
+        log('SELECT BIG DATA rows length: ' + results[2].rows.length)
+        log('EXTRA SELECT 2 result: ' + JSON.stringify(results[3]))
+        extraCheck2()
+      }
+    )
+  }
+
+  function extraCheck2 () {
+    log('EXTRA CHECK 2')
+    window.sqliteBatchConnectionManager.executeBatch(
+      connectionId,
+      [
+        ['SELECT COUNT(*) FROM BIG', []],
+        ['SELECT UPPER(?)', ['Extra test 1']],
+        ['SELECT DATA FROM BIG', []]
+      ],
+      function (results) {
+        log('SELECT COUNT results: ' + JSON.stringify(results[0]))
+        log('Extra test 1 result: ' + JSON.stringify(results[1]))
+        log('SELECT BIG DATA rows length: ' + results[2].rows.length)
+        extraCheck3()
+      }
+    )
+  }
+
+  function extraCheck3 () {
+    log('EXTRA CHECK 3')
+    window.sqliteBatchConnectionManager.executeBatch(
+      connectionId,
+      [['SELECT DATA FROM BIG', []]],
+      function (results) {
+        log('SELECT BIG DATA OK')
+        log('SELECT BIG DATA rows length: ' + results[0].rows.length)
+      }
+    )
+  }
+}
---
+++ src/android/SQLiteBatchConnectionManager.java
@@ -0,0 +1,375 @@
+// Copyright 2020-present Christopher J. Brody <chris.brody+brodybits@gmail.com>
+
+package io.sqlc;
+
+import java.util.Vector;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class SQLiteBatchConnectionManager extends CordovaPlugin {
+  static private final int JSON_RESULTS_ROUGH_CUTOFF_SIZE = 5*1000*1000;
+
+  static class BatchData implements SQLiteBatchCore.BatchData {
+    BatchData (JSONArray data) {
+      this.data = data;
+    }
+
+    @Override
+    public int getEntryCount() {
+      try {
+        return data.length();
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public void openEntry(int index) {
+      try {
+        entry = data.getJSONArray(index);
+        bind = entry.getJSONArray(1);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public String getEntryStatement() {
+      try {
+        return entry.getString(0);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public int getEntryBindColumnCount() {
+      try {
+        return bind.length();
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public boolean isEntryBindColumnNumber(int column) {
+      try {
+        // TBD CACHE THIS VALUE:
+        final Object o = bind.get(column);
+
+        return (o instanceof Number);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public boolean isEntryBindColumnString(int column) {
+      try {
+        // TBD CACHE THIS VALUE:
+        final Object o = bind.get(column);
+
+        return (o instanceof String);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public double getEntryBindColumnDouble(int column) {
+      try {
+        return bind.optDouble(column);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public String getEntryBindColumnString(int column) {
+      try {
+        return bind.optString(column);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    JSONArray data;
+    JSONArray entry;
+    JSONArray bind;
+  }
+
+  static class BatchResults implements SQLiteBatchCore.BatchResults {
+    BatchResults(int totalResultCount, CallbackContext cbc) {
+      this.totalResultCount = totalResultCount;
+      this.cbc = cbc;
+      this.jsonResults = new Vector<String>(totalResultCount);
+      this.jsonResultsRoughSize = 0;
+    }
+
+    @Override
+    public void startNewEntry() {
+      result = new JSONObject();
+    }
+
+    @Override
+    public void entryPutFieldAsInteger(String name, int value) {
+      try {
+        result.put(name, value);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public void entryPutFieldAsString(String name, String value) {
+      try {
+        result.put(name, value);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public void putNewEntry() {
+      String jsonResult = this.result.toString();
+
+      this.jsonResults.add(jsonResult);
+      this.jsonResultsRoughSize += jsonResult.length();
+      ++this.resultCount;
+      if (this.resultCount == this.totalResultCount) this.sendResults();
+    }
+
+    @Override
+    public void startNewEntryWithRows() {
+      result = new JSONObject();
+      columns = new JSONArray();
+      this.jsonRows = new Vector<String>();
+      this.jsonRowsRoughSize = 0;
+    }
+
+    @Override
+    public void entryPutColumnName(String columnName) {
+      columns.put(columnName);
+    }
+
+    @Override
+    public void startEntryRow() {
+      row = new JSONArray();
+    }
+
+    @Override
+    public void entryPutRowValueAsString(String value) {
+      row.put(value);
+    }
+
+    @Override
+    public void entryPutRowValueAsDouble(double value) {
+      try {
+        row.put(value);
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    @Override
+    public void entryPutRowValueAsNull() {
+      row.put(JSONObject.NULL);
+    }
+
+    @Override
+    public void entryPutRow() {
+      String jsonRow = this.row.toString();
+      this.jsonRows.add(jsonRow);
+      this.jsonRowsRoughSize += jsonRow.length();
+    }
+
+    @Override
+    public void putNewEntryWithRows() {
+      try {
+        if (this.jsonRowsRoughSize < JSON_RESULTS_ROUGH_CUTOFF_SIZE) {
+          // String.join() seems to be not working on
+          // all supported Android versions
+          String jsonResult =
+            "{" +
+            "\"status\"" + ":" + "0" + "," +
+            "\"columns\"" + ":" + columns.toString() + ","+
+            "\"rows\"" + ":" + this.jsonRows +
+            "}";
+
+          this.jsonResults.add(jsonResult);
+          this.jsonResultsRoughSize += jsonResult.length();
+          ++this.resultCount;
+          if (this.resultCount == this.totalResultCount) this.sendResults();
+
+          return;
+        }
+
+        // else ...
+
+        this.sendResults();
+
+        final int rowsLength = jsonRows.size();
+
+        result.put("status", 0); // SQLite OK
+        result.put("partial", 1);
+        result.put("columns", columns);
+        result.put("rowsLength", rowsLength);
+
+        ++resultCount;
+        PluginResult pr = new PluginResult(PluginResult.Status.OK, result);
+        pr.setKeepCallback(true);
+        cbc.sendPluginResult(pr);
+
+        for (int i=0; i<rowsLength; ++i) {
+          pr = new JSONPluginResult(jsonRows.get(i));
+          if (resultCount != totalResultCount || i < rowsLength)
+            pr.setKeepCallback(true);
+          cbc.sendPluginResult(pr);
+        }
+      } catch(Exception e) {
+        // NOT EXPECTED - internal error:
+        throw new RuntimeException(e);
+      }
+    }
+
+    private void sendResults() {
+      int sendCount = this.jsonResults.size();
+
+      StringBuilder jsonBuilder = new StringBuilder();
+      jsonBuilder.append('[');
+      for (int i=0; i < sendCount; ++i) {
+        if (i != 0) jsonBuilder.append(',');
+        jsonBuilder.append(this.jsonResults.get(i));
+      }
+      jsonBuilder.append(']');
+
+      PluginResult pr = new JSONPluginResult(jsonBuilder.toString());
+      if (this.resultCount != this.totalResultCount) pr.setKeepCallback(true);
+      cbc.sendPluginResult(pr);
+
+      this.jsonResults = new Vector<String>(totalResultCount);
+      this.jsonResultsRoughSize = 0;
+    }
+
+    private class JSONPluginResult extends PluginResult {
+      JSONPluginResult(String json) {
+        super(PluginResult.Status.OK);
+        this.jsonString = json;
+      }
+
+      @Override
+      public int getMessageType() {
+        return PluginResult.MESSAGE_TYPE_JSON;
+      }
+
+      @Override
+      public String getMessage() {
+        return jsonString;
+      }
+
+      private String jsonString;
+    }
+
+    int totalResultCount;
+
+    CallbackContext cbc;
+
+    int resultCount;
+
+    Vector<String> jsonResults;
+
+    int jsonResultsRoughSize;
+
+    JSONObject result;
+
+    JSONArray columns;
+
+    Vector<String> jsonRows;
+
+    int jsonRowsRoughSize;
+
+    JSONArray row;
+  }
+
+  @Override
+  public boolean execute(String method, JSONArray data, CallbackContext cbc) {
+    switch(method) {
+      case "openDatabaseConnection":
+        openDatabaseConnection(data, cbc);
+        break;
+      case "executeBatch":
+        executeBatch(data, cbc);
+        break;
+      default:
+        return false;
+    }
+    return true;
+  }
+
+  static private void
+  openDatabaseConnection(JSONArray args, CallbackContext cbc) {
+    try {
+      final JSONObject options = args.getJSONObject(0);
+
+      final String fullName = options.getString("fullName");
+
+      final int flags = options.getInt("flags");
+
+      final int mydbc = SQLiteBatchCore.openBatchConnection(fullName, flags);
+
+      if (mydbc < 0) {
+        cbc.error("open error: " + -mydbc);
+      } else {
+        cbc.success(mydbc);
+      }
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
+  }
+
+  static private void executeBatch(JSONArray args, CallbackContext cbc) {
+    try {
+      final int mydbc = args.getInt(0);
+
+      JSONArray data = args.getJSONArray(1);
+
+      // Background threading is under future consideration at this point
+      // (expected to be straightforward for both Android & iOS)
+      executeBatchNow(mydbc, data, cbc);
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
+  }
+
+  static private void
+  executeBatchNow(final int mydbc, JSONArray data, CallbackContext cbc) {
+    try {
+      JSONArray results = new JSONArray();
+
+      SQLiteBatchCore.executeBatch(mydbc,
+        new BatchData(data),
+        new BatchResults(data.length(), cbc));
+    } catch(Exception e) {
+      // NOT EXPECTED - internal error:
+      cbc.error(e.toString());
+    }
+  }
+}
